819e4dfe6dfebe3cb80e55f2de05a1abe5a0b1c4
Ryan Esteves <snargledorf@gmail.com>
2012-01-20 17:29:57 -0500

    Cleaned up the header file

--- WakeOnLan.h
+++ WakeOnLan.h
@@ -11,34 +11,37 @@
 #ifndef wakeonloan_h
 #define wakeonloan_h
 
-#if defined(ARDUINO) && ARDUINO >= 100
+#if defined(ARDUINO) && ARDUINO >= 100 // Arduino 1.0+ compatibility
+
 # include "EthernetUdp.h"
 # include "Arduino.h"
-#else
-# include "Udp.h"
-# include "WProgram.h"
-#endif
-
 class WakeOnLan {
 	public:
-#if defined(ARDUINO) && ARDUINO >= 100
 		WakeOnLan(EthernetUDP Upd);
 		// Send a magic packet to the provided mac address on the specified port
 		void send(byte* mac, byte port);
+		
+	private:
+		EthernetUDP _udp;
+		byte _ip[4];
+		byte _magic_packet[102];
+};
+
 #else
+
+# include "Udp.h"
+# include "WProgram.h"
+class WakeOnLan {
+	public:
 		WakeOnLan();	
 		// Send a magic packet to the provided mac address on the specified port
 		static void send(byte* mac, byte port);
-#endif
-
 		
 	private:
-#if defined(ARDUINO) && ARDUINO >= 100
-		EthernetUDP _udp;
-#endif
 		byte _ip[4];
-		byte _magic_packet[102];
-		
+		byte _magic_packet[102];		
 };
+
+#endif
 
 #endif
